4868ce53d24168ce93ccd4959313f450f2e82fea
Jefmamklyo <9937145@schoolsnet.act.edu.au>
2026-03-01 20:56:38 +1100

    added multi-threading however this lacks the proper facilities to prevent race conditions

--- CameraSetup.py
+++ CameraSetup.py
@@ -1,7 +1,8 @@
 #import cv library and assign to variable cv
+#Import threading
 import cv2 as cv
+import threading
 
-#Define camera
 
 #Encapsulation class
 class CamManage:
@@ -10,13 +11,43 @@ class CamManage:
         self.cam = cv.VideoCapture(0)
         self.width = width
         self.height = height
+        #---------Threading shared variables __________#
+
+        self.frame = None #Stores captured frame and starts empty becuase no captured frame
+        self.isRunning = False  #Thread loop running controls
+      
+    def start(self):
+        self.isRunning = True
+        #Creates a 1 new daemon thread which will run the function update located within self or the class
+        self.thread = threading.Thread(target = self.update, daemon=True)
+        self.thread.start() #Starts the thread
     
-    #Setting/Further optimisation
+    #defines what happens in the new thread
+    def update(self):
+        #reads cam and stores it in shared variable frane
+        while self.isRunning:
+            ret, frame = self.cam.read()
+            #if ret is true/ stream exists
+            if ret:
+                self.frame= frame
+        
+    #returnlatest frame stored in self.frame
+    def read(self):
+        return self.frame
+
+    #frame processing
     def setting(self, frame):
         frame = cv.resize(frame, (self.width,self.height))
         frame =cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         return frame
          #---------optimisation  code go here __________
+
+    def stop(self):
+        self.isRunning = False
+        #halt the main thread so that the second thread we created stops before it executes again
+        self.thread.join()
+        self.cam.release()
+
 
 
 #Instantiating the class
@@ -27,23 +58,24 @@ if not manager.cam.isOpened():
     print("Camera not connected")
     exit()
 
+manager.start() #starts running the new thread
 
 
+#Main thread
 while True:
-    #Capture each frame and return information (retrn)
-    retrn, frame = manager.cam.read()
+    frame = manager.read()
 
-    #Calling setting
-    frame = manager.setting(frame)
+    #Optimiser
+    optimiser = manager.setting(frame)
     
-
     #display
-    cv.imshow('Video',frame)
+    cv.imshow('Video', optimiser)
+
     #exit
     exitKey= cv.waitKey(1)
     if exitKey == ord('l'):
         break
     
 #exit sequence
-cv.release()
+manager.stop() #automatically releases the camera
 cv.destroyAllWindows()
