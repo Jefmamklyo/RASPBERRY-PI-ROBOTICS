ec4a1ef757c9b6e43ada94229e02319679d17505
Jefmamklyo <9937145@schoolsnet.act.edu.au>
2026-03-01 16:11:31 +1100

    First iteration of camera code to get it working

--- CameraSetup.py
+++ CameraSetup.py
@@ -1,2 +1,26 @@
-#import cv library
-import cv2 
+#import cv library and assign to variable cv
+import cv2 as cv
+ 
+cam = cv.VideoCapture(0)
+
+#Null checks
+if not cam.isOpened():
+    print("Error: Canmera not connected")
+    exit()
+
+while True:
+    #Capture each frame and return information (retrn)
+    retrn, frame = cap.read()
+
+    
+    #---------optimisation  code go here __________#
+
+    #display
+    
+    exitKey= cv.waitKey(1)
+    if exitKey == ord('l'):
+        break
+    
+#exit sequence
+cv.release()
+cv.destroyAllWindows()
